cc6aa75dbe42b19abf6be730186af46c728259d0
m1nzez <vivi020405@naver.com>
2024-04-02 14:59:35 +0900

    day1

---
+++ UMC_01/umc_01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  umc_01
+//
+//  Created by 김민지 on 3/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
