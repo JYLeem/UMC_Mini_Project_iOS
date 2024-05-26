100168a0efd8711c5f928dd65ab2e8dba488c41a
m1nzez <vivi020405@naver.com>
2024-05-26 18:49:45 +0900

    Add Git LFS tracking for large files

---
+++ miniProject-home/miniProject-home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniProject-home
+//
+//  Created by 김민지 on 5/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
