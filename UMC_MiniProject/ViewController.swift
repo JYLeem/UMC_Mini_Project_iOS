1d0b8232109dfbd32bba1270451c66b7c8490cc7
JYLeem <jinyoungim105@gmail.com>
2024-06-17 13:22:13 +0900

    Initial Commit

---
+++ UMC_MiniProject/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  UMC_MiniProject
+//
+//  Created by Jinyoung Leem on 6/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
